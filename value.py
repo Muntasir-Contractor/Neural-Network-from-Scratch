33d9f921f47704ac3628f03998a7da78e172d3ab
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-04-24 19:28:56 -0400

    Allow Value to perform operations with float or int types. Added subtraction as an operation

--- value.py
+++ value.py
@@ -1,27 +1,42 @@
 class Value:
-    def __init__(self, data, _children=(), _op=""):
+    def __init__(self, data, _children=(), _op="",gradient=0.0):
         self.data = data
         self.prev = set(_children)
         self._op = _op
+        self.gradient = gradient
+        self._backward = lambda:None
+
 
 
     def __repr__(self):
         return f"Value(data={self.data})"
     
     def __add__(self, other):
+        if not isinstance(other, Value):
+            return Value(self.data + other, (self,other), "+")
+
         return Value(self.data + other.data,(self,other),"+")
     
     def __mul__(self,other):
+        if not isinstance(other,Value):
+            return Value(self.data * other, (self,other), "*")
         return Value(self.data * other.data, (self,other),"*")
+    
+    def __sub__(self,other):
+        if not isinstance(other,Value):
+            return Value(self.data - other, (self,other), "-")
+        return Value(self.data - other.data, (self,other),"-")
+    
+    def __pow__(self,other):
+        pass
+
+
     
 
 a = Value(4)
 b = Value(3)
 c = a+b
-d = a*b
-e = c+d
+d = c + 3
 
-print(c)
-print(c.prev)
-print(c._op)
-print(e.prev)
+print(d)
+print(d.prev)
