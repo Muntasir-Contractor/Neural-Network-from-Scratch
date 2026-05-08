61c82b00dce27add5a080636d2e688cc237c12cf
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-05-07 21:22:34 -0400

    Implemented fit and predict function. fit performs batch gradient descent across an 'epochs' amount of times, readjusting the weights given the learning rate and gradient

--- neuralnet.py
+++ neuralnet.py
@@ -10,6 +10,9 @@ class Neuron:
         activation = sum((wi*xi for wi,xi in zip(self.w,x))) + self.b
         out = activation.tanh()
         return out
+    
+    def parameters(self):
+        return self.w + [self.b]
 
 class Layer:
     def __init__(self, nin,nout):
@@ -17,21 +20,48 @@ class Layer:
 
     def __call__(self,x):
         outs = [n(x) for n in self.neurons]
-        return outs
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        params = []
+        for neurons in self.neurons:
+            ps = neurons.parameters()
+            params.extend(ps)
+        return params
+        
 
 class MLP:
 
     def __init__(self, nin, nouts):
         sizes = [nin] + nouts
         self.layers = [Layer(sizes[i], sizes[i+1]) for i in range(len(nouts))]
+        self.loss = None
 
     def __call__(self, x):
         for layer in self.layers:
             x = layer(x)
         return x
     
-    def predict(self,xs,ys):
-        pass
+    def predict(self,xs):
+        ypred = [self(x) for x in xs]
+        return ypred
+
+    def fit(self, xs,ys, lr=0.01, epochs=1000):
+        for epoch in range(epochs):
+            for p in self.parameters():
+                p.grad = 0.0
+            ypred = [self(x) for x in xs]
+            self.loss = sum((yout - ygt)**2 for ygt,yout in zip(ys,ypred))
+            self.loss.backward()
+
+            for p in self.parameters():
+                p.data += -lr * p.grad
+        
+        return self.loss.data
+
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
 
 
 xs = [
@@ -43,5 +73,5 @@ xs = [
 ]
 n = MLP(3, [4,4,1])
 ys = [1.0,-1.0,-1.0,1.0]
-ypred = [n(x) for x in xs]
-print(ypred)
+n.fit(xs,ys)
+print(n.predict(xs))
